1bb379134d8dca480f63cfdc2ee615d22406ea8e
Juraj Panoc <juraj.panoc@gmail.com>
2024-12-07 00:33:37 +0100

    added pop and append macros to the file, added a printerr macro that prints an error message to the stderr

--- list.h
+++ list.h
@@ -34,7 +34,7 @@ typedef struct { \
 //alternate syntax for accessing array elements (no name->data[index] needed)
 #define iget(name, index) name->data[index]
 
-
+#define printerr(msg) fprintf(stderr, msg)
 ////////////////////
 // list "methods" /////////////////////////////////////////////////////////////////////////////////
 ////////////////////
@@ -49,7 +49,7 @@ typedef struct { \
     ident->data = malloc(sizeof(type) * DEFAULT_SIZE); \
     if(ident->data == NULL) exit(EXIT_FAILURE);\
     ident->size = DEFAULT_SIZE; \
-    ident->len = 0;
+    ident->len = 0
 
 /*
 * deallocates the memory tied to the list that is passed in,
@@ -58,18 +58,61 @@ typedef struct { \
 #define list_dtor(list) \
     free(list->data); \
     free(list); \
-    list = NULL;
+    list = NULL
 
 /*
 * prints out the data that is in the list,
 * format_str sets the way it is printed (the way the type is interpretted)
 */
-#define print_list(list_ptr, format_str) \
-    printf("[" format_str ", ", list_ptr->data[0]);\
-    for(size_t i = 1; i < list_ptr->len - 1; i++){ \
-        printf(format_str ", ", list_ptr->data[i]); \
+#define print_list(list, format_str) \
+    printf("[" format_str ", ", list->data[0]);\
+    for(size_t i = 1; i < list->len - 1; i++){ \
+        printf(format_str ", ", list->data[i]); \
     } \
-    printf(format_str "]\n", list_ptr->data[list_ptr->len - 1]);
+    printf(format_str "]\n", list->data[list->len - 1])
 
+
+/*
+* doubles the size of the array and reallocates the memory,
+* crashes the program if the reallocation failed
+
+* (uses __typeof__ insted of typeof to avoid compiler warnings)
+*/
+#define list_reallocator(list) \
+    list->size *= 2; \
+    list->data = realloc(list->data, list->size * sizeof(__typeof__(list->data[0]))); \
+    if(list->data == NULL) exit(EXIT_FAILURE)
+
+
+/*
+* adds an item to the end of the array and increments its length,
+* if its about to exceed its size then it reallocs a new bigger array
+*/
+#define append(list, item) \
+    if(list->len >= list->size - 1) { \
+        list_reallocator(list); \
+    } \
+    list->data[list->len++] = item
+
+
+/*
+* pops the last item out and decrements the length variable,
+* if the list is empty then it prints "list is empty, cant pop more items" 
+  to stderr and crashes the program
+
+  (parentheses are used in the definition so it can return the popped item value
+  a.k.a the last line in the definition)
+*/
+#define pop(list) \
+    ({ \
+        __typeof__(list->data[0]) popped;\
+        if (list->len == 0){ \
+            printerr("list is empty, cant pop more items"); \
+            exit(EXIT_FAILURE); \
+        } \
+        popped = list->data[list->len - 1]; \
+        list->len--; \
+        popped;\
+    })
 
 #endif
--- main.c
+++ main.c
@@ -12,32 +12,32 @@ void outside_scope(list_str* z){
 }
 
 int main(){
-    list_ctor(x, int)
-    list_ctor(y, char)
-    list_ctor(z, str)
+    list_ctor(x, int);
+    list_ctor(y, char);
+    list_ctor(z, str);
 
-    z->len = 2;
-    iget(z, 0) = "Hello"; //alternate syntax for accessing array elements (no -> needed)
-    z->data[1] = "World";
-    
+    append(z, "Hello");
+    append(z, "World");
+
     outside_scope(z);
+
+    append(x, 4);
+    append(x, 1);
+    append(x, 7);
+
+    append(y, 'a');
+    append(y, 'h');
+    append(y, 'a');
+
+    char res = pop(y); //fix this buggy c extention behavior (compiler is fine with it just the extention doesn't like it)
     
-    y->len = 3;
-    x->len = 3;
+    printf("popped item: %c\n", res);
+    print_list(x, "%d");
+    print_list(y, "%c");
+    print_list(z, "%s");
     
-    x->data[0] = 4;
-    x->data[1] = 1;
-    x->data[2] = 7;
-    y->data[0] = 'a';
-    y->data[1] = 'h';
-    y->data[2] = 'a';
-    
-    print_list(x, "%d")
-    print_list(y, "%c")
-    print_list(z, "%s")
-    
-    list_dtor(x)
-    list_dtor(y)
-    list_dtor(z)
+    list_dtor(x);
+    list_dtor(y);
+    list_dtor(z);
     return 0;
 }
